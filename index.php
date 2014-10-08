850008e5b79a769bfe5c8d39790ab989c7d93a36
apao87 <apao24@gmail.com>
2014-10-08 10:08:14 +0200

    v1

---
+++ index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<title>Video nouvelles</title>
+		<link rel="stylesheet" href="main.css" />
+	</head>
+	<body background="images/fondo.jpg">
+		<div id="articulos">
+			<header id="top_header">
+				<h1>Video nouvelles</h1>
+			</header>
+			<nav id="top_menu">
+				<ul>
+					<li>Inicio</li>
+					<li>PS4<img src="images/ps4.jpg" width="30px" height="20px"></li>
+					<li>XBOX ONE<img src="images/xboxone.jpg" width="30px" height="20px"></li>
+					<li>WII U<img src="images/wiiu.png" width="30px" height="20px"></li>
+				</ul>
+			</nav>
+			
+			<aside id="left_aside">
+				<h4><u>Noticias</u></h4>
+				<br>
+				<img src="images/the_last_guardian.jpg" width="200" height="100" id="the_last_guardian">
+				<p><h5>Shuhei Yoshida: 'El desarrollo de The Last Guardian no podría ir mejor'</h5>
+				</p>
+				<br>
+				<img src="images/the_witcher.jpg" width="200" height="100" id="the_witcher">
+				<p><h5>Geralt de Rivia se deja ver una vez más en las nuevas imágenes de The Witcher 3: Wild Hunt
+				</h5></p>
+				<br>
+				<img src="images/win10.jpg" width="200" height="100" id="win10">
+				<p><h5>El equipo de DirectX ha anunciado a través de su blog oficial que la nueva iteración de sus librerías llegará con Windows 10 en algún momento del próximo año 2015.
+				</h5></p>
+				<br>
+				<img src="images/metal.jpg" width="200" height="100" id="metalGear">
+				<p><h5>La espera de los usuarios de PC para Metal Gear Solid V: Ground Zeroes estaría cerca de llegar a su fin. Según apunta medios de comunicación rusos, el título de Hideo Kojima estrenará versión en compatibles el 18 de diciembre. 
+				</h5></p>
+			</aside>
+			<aside id="right_aside">
+				<h4><u>Análisis</u></h4>
+				<br>
+				<img src="images/mordor.jpg" width="200" height="100" id="mordor">
+				<h5><b>La tierra media:Las sombras de Mordor</b></h5>
+				<p><h5>Ya tenemos aquí una más de las producciones para consolas Next-Gen y PC basadas en el atractivo mundo de El Señor de los Anillos.</h5></p>
+				<br>
+				<img src="images/supersmash.jpg" width="200" height="100" id="supersmash">
+				<h5><b>Super smash bros</b></h5>
+				<p><h5>Nintendo acaba, por fin, de poner en el mercado su nueva y esperadísima entrega de la serie Super Smash Bros.</h5></p>
+				<br>
+				<img src="images/fifa.jpg" width="200" height="100" id="fifa">
+				<h5><b>Fifa 15</b></h5>
+				<p><h5>Hay algunos cambios importantes en el episodio de este año, y van más allá de los cacareados porteros.</h5></p>
+				<br>
+				<img src="images/alien.jpg" width="200" height="100" id="alien">
+				<h5><b>Alien isolation</b></h5>
+				<p><h5>Una criatura monstruosa frente a una mujer. Un alienígena contra una humana. Alien vs Ripley. </h5></p>
+			</aside>
+			<section id="main_section">
+				<article>
+					<header>
+						<hgroup>
+							<br>
+							<h1>El Héroe</h1>
+							<br>
+								<div style="text-align:center;" ><img src="images/the_witcher3.jpg" width="700" height="300" id="imagen_main_section1"></div>
+							<h6>5 de Octubre de 2014</h6>
+						</hgroup>
+					</header>
+					<br>
+					<br>
+				<p><h5>
+				Geralt de Rivia es un auténtico tipo duro. Su personaje, fruto de la obra de Andrzej Sapkowski del mismo nombre, ha sido trazado con maestría a lo largo de la serie de libros, y también en paralelo y con algunas diferencias por CD Projekt Red en su trilogía de videojuegos. Este es, en principio, el último lanzamiento sobre el brujo que va a llevar a cabo el estudio polaco, así que esperamos un cierre para su trayecto a la altura, y también un continuado desarrollo de su personalidad al nivel de lo que hemos venido viendo hasta la fecha. 
+				</h5></p>
+				<nav id="sub1_menu">
+				<ul>
+					<li>Más info</li>
+					<li>Opinión</li>
+				</ul>
+				</nav>
+				<br>
+				<br>
+				<article>
+					<header>
+						<hgroup>
+							<h1>El mundo abierto</h1>
+							<br>
+								<div style="text-align:center;" ><img src="images/the_witcher2.jpg" width="700" height="300" id="imagen_main_section2"></div>
+							<h6>5 de Octubre de 2014</h6>
+						</hgroup>
+					</header>
+					<br>
+					<br>
+				<p><h5>Wild Hunt supone la primera vez que la saga The Witcher III aborda un mundo abierto dentro del que nos vamos a poder mover con total libertad. Las dimensiones de éste son sencillamente asombrosas, incorporándose a la fórmula caballos, barcos y otros medios de transporte que podremos emplear para hacer más llevaderos nuestros frecuentes desplazamientos. Ciudades, enormes zonas naturales y, también, insondables mares que podremos explorar con libertad o con la obligación de cumplir exigentes misiones. CD Projekt Red está dispuesto, con su tamaño y densidad de cosas por hacer, a dejar en pañales a maestros como Bethesda Softworks.
+				</h5></p>
+				</article>
+				<nav id="sub2_menu">
+					<ul>
+						<li>Más info</li>
+						<li>Opinión</li>
+					</ul>
+				</nav>
+				</section>
+				<br>
+			<footer id="the_footer">
+			Copyright Josu 2014
+			</footer>
+		</div>
+	</body>
+</html>
